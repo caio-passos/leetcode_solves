0aacbc81c16cfd5206d1b3e2a44b307d9d32d62e
Caio Passos <caiopss99@gmail.com>
2023-03-13 19:33:02 +0000

    The main objectives of this task

---
+++ 13_roman_to_integer/romanDigits.java
@@ -0,0 +1,11 @@
+package digits;
+
+public class romanDigits{
+    private int I = 1;
+    private int V = 5;
+    private int X = 10;
+    private int L = 50;
+    private int C = 100;
+    private int D = 500;
+    private int M = 1000;
+}
---
+++ 13_roman_to_integer/romanToInteger.java
@@ -0,0 +1,39 @@
+/* solve 
+Given a roman numeral, convert it to an integer.
+
+Symbol       Value
+I             1
+V             5
+X             10
+L             50
+C             100
+D             500
+M             1000
+
+Constraints:
+
+    1 <= s.length <= 15
+    s contains only the characters ('I', 'V', 'X', 'L', 'C', 'D', 'M').
+    It is guaranteed that s is a valid roman numeral in the range [1, 3999].
+    
+
+    What the code should do:
+        1- User input as String
+        2- Make it interper each digit as the sum of '3' consecutive equals digits and or,
+        a single non consecutive digit.
+        3- Conditionals to limit the input to max of '3' consecutive equal strings(ex. III),
+        4- Conversion of this String as it's related integer values in roman numeral
+
+*/
+package digits;
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+public class romanToInteger{
+
+    
+    public static void main(String[] args) {
+        JOptionPane.showInputDialog(null, "Type a roman number to convert to an integer value:");
+    
+    }
+}
